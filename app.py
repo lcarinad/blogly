3c5800b5f46f538b43a15835b432608eb3a3b0be
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-10-27 13:56:56 -0400

    new user route form, post request

--- app.py
+++ app.py
@@ -17,10 +17,32 @@ app.app_context().push()
 connect_db(app)
 
 @app.route('/')
+def home():
+    return redirect("/users")
+
+@app.route('/users')
 def list_users():
     """Shows list of users in db"""
     users = User.query.all()
     return render_template("list.html", users=users)
+
+@app.route('/users/new')
+def add_user():
+    """shows add new user form"""
+    return render_template("userform.html")
+
+@app.route('/users/new', methods=["POST"])
+def submit_new_user():
+    """taking user input from form, send to users db, and redirect to /users"""
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url"]
+    
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/users")
 
 @app.route('/<int:user_id>')
 def show_user(user_id):
